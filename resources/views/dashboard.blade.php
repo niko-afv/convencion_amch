e8b49ae428d843319ed5af5de3127def1fd63408
Nicolás Fredes <niko.afv@gmail.com>
2015-05-19 00:11:12 -0300

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,7 +40,7 @@
 
                                 @foreach($categorias as $categoria)
                                     <div class="col-lg-4">
-                                        <?php print_r($fotos_count[$categoria->NOMBRE]);die;?>
+                                        <?php print_r($desafio);die;?>
                                         @if($fotos_count[$categoria->NOMBRE] ==  0)
                                             <a href="/formulario/{{ $categoria->ID }}">
                                          @else
